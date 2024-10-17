52f316ce14b241fb757fb2fa579f16991e9a4d52
Symon Kipkemei <symonkipkemei@gmail.com>
2024-10-17 03:11:34 +0300

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Squirrel;
+
+namespace SquirrrelHouse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AddVersionNumber();
+
+            CheckForUpdates();
+        }
+
+
+        private async Task CheckForUpdates()
+        {
+            using (var manager = new UpdateManager(@"C:\Users\Symon Kipkemei\Desktop\Temp\Releases")) //References the server
+            {
+                await manager.UpdateApp();
+            }
+        }
+
+        private void AddVersionNumber()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+
+            string msg = "Hello Welcome.\n Squirrel in the building ";
+            string message = $" v.{fileVersionInfo.FileVersion}";
+
+            this.Title += message;
+            
+            tbGreetings.Text = msg + message;
+        }
+    }
+}
